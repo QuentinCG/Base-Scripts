1da798926d20ac7d687e6a0de69418f905f06d7f
Quentin Comte-Gaz <quentin@comte-gaz.com>
2016-08-03 21:40:58 +0200

    linux: Add basic commands to be loaded at every session

---
+++ Linux/basic_commands.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+#@brief Some commands to make Linux daily use easier
+#@author Quentin Comte-Gaz <quentin@comte-gaz.com>
+#@date 3 August 2016
+#@license MIT License (contact me if too restrictive)
+#@copyright Copyright (c) 2016 Quentin Comte-Gaz
+#
+#@note Put those commands in ~/.bashrc to use them after login
+
+# Auto-correct "cd" errors with dir names
+shopt -s cdspell
+
+# Display a 80 characters line between every command in the shell
+export PS1="_______________________________________________________________________________\n\u@\h:\w#"
