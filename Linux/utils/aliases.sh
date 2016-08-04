b19f01e00db08a1b993a63c7b812c2d7d7ae4c7d
Quentin Comte-Gaz <quentin@comte-gaz.com>
2016-08-04 18:52:33 +0200

    linux: Add more aliases (and delete lr alias)

--- Linux/utils/aliases.sh
+++ Linux/utils/aliases.sh
@@ -17,8 +17,23 @@ alias ..4="cd ../../../.."
 alias ..5="cd ../../../../.."
 alias ..6="cd ../../../../../.."
 
-# Go to home directory
+alias ...="cd ../.."
+alias ....="cd ../../.."
+alias .....="cd ../../../.."
+alias ......="cd ../../../../.."
+alias .......="cd ../../../../../.."
+
+# Set vim as default text editor
+alias vi="vim"
+alias edit="vim"
+
+# Try to continue download before normal download
+alias wget='wget -c'
+
+# Directory shortcuts
+alias home="cd ~/"
 alias ~="cd ~"
+alias localhost="cd /var/www"
 
 # List files and folders
 alias ll="ls -FGlAhp"
@@ -29,12 +44,25 @@ alias c="clear"
 # Show all executable paths
 alias path="echo -e ${PATH//:/\\n}"
 
-# Full recursive directory listing
-alias lr='ls -R | grep ":$" | sed -e '\''s/:$//'\'' -e '\''s/[^-][^\/]*\//--/g'\'' -e '\''s/^/   /'\'' -e '\''s/-/|/'\'' | less'
-
 # Find all files in current sub-folders containing specific string
 alias findIn="find . -type f -print0 | xargs -0 grep -l"
 
 # Git
 alias push="git push origin master"
 alias pull="git pull"
+
+# Spelling typos
+alias xs='cd'
+alias vf='cd'
+alias moer='more'
+alias moew='more'
+alias kk='ll'
+alias mm='ll'
+
+# Show IP
+alias myIp="curl ipinfo.io/ip"
+
+# Apt
+alias update="apt-get update"
+alias upgrade="apt-get upgrade"
+#alias install="apt-get install" # (already in functions)
