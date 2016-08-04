261acc082963173062c4ff13104e6853538c9d55
Quentin Comte-Gaz <quentin@comte-gaz.com>
2016-08-04 20:58:30 +0200

    linux: Improve bashrc and bash profile install
    Make sure bash profile and bashrc install can be done
    from an other script and for any user.

--- Linux/bash/README.md
+++ Linux/bash/README.md
@@ -1,12 +1,19 @@
 # What is it?
 
-Those files are scripts that should be called during loggin
+".bashrc" and ".bash_profile" are scripts that should be called during loggin
 or when a terminal is launched.
+
 
 # How to install
 
 Launch this script:
 ```shell
-chmod 750 install.sh
-./install.sh
+chmod 755 install.sh
+./install.sh PATH_TO_STORE_BASH_SCRIPTS
+```
+
+Example:
+```shell
+chmod 755 install.sh
+./install.sh /home/perfect_user
 ```
--- Linux/bash/install.sh
+++ Linux/bash/install.sh
@@ -1,11 +1,17 @@
 #!/usr/bin/env bash
 
-echo "Setting the bash profile to the current user"
-mv bash_profile ~/.bash_profile
+#@brief Set the bash profile and bashrc in path $1
+#@arg $1 path Path where bashrc and bash profile will be stored
 
-echo "Setting the bashrc to the current user"
-mv bashrc ~/.bashrc
+BASEDIR=$(dirname "$0")
 
 echo "Copying the aliases,commands and functions to /etc/utils/"
+sudo rm -rf /etc/utils/
 sudo mkdir /etc/utils
-sudo mv ../utils/* /etc/utils/
+sudo cp "$BASEDIR"/../utils/* /etc/utils/
+
+echo "Setting the bash profile to the current user"
+sudo cp "$BASEDIR"/bash_profile "$1"/.bash_profile
+
+echo "Setting the bashrc to the current user"
+sudo cp "$BASEDIR"/bashrc "$1"/.bashrc
