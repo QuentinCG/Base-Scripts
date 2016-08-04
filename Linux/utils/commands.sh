407d0eb923f4828b0223bfcecc7801d624c5af39
Quentin Comte-Gaz <quentin@comte-gaz.com>
2016-08-04 18:54:39 +0200

    linux: Add bash completion and mail errors disabling cmd

--- Linux/utils/commands.sh
+++ Linux/utils/commands.sh
@@ -11,5 +11,15 @@
 # Auto-correct "cd" errors with dir names
 shopt -s cdspell
 
+# Disable shell mail warnings
+shopt -u mailwarn
+unset MAILCHECK
+
 # Display a 80 characters line between every command in the shell
 export PS1="________________________________________________________________________________\n\u@\h:\w$ "
+#export PS1="__________________________$(date +'%A %W %Y %X')__________________________\n\u@\h:\w$ "
+
+# Bash completion
+if [ -f /etc/bash_completion ]; then
+  source /etc/bash_completion
+fi
