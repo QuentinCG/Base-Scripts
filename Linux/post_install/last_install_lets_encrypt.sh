0dab1d0d07b1e913da95d9ede88a1ba98fd1a55d
Quentin Comte-Gaz <quentin@comte-gaz.com>
2017-07-01 08:40:39 +0200

    Linux: Install Let's Encrypt (post install)

---
+++ Linux/post_install/last_install_lets_encrypt.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+#@brief Post install last step:
+#  - Install and configure Let's Encrypt
+#  - Add auto renew cert with Let's Encrypt
+#
+# This script must be called by a sudo user
+#
+#@author Quentin Comte-Gaz <quentin@comte-gaz.com>
+#@date 1 July 2017
+#@license MIT License (contact me if too restrictive)
+#@copyright Copyright (c) 2017 Quentin Comte-Gaz
+
+source ../utils/functions.sh
+
+echo "--------- Update and upgrade the system ----------"
+updateAndUpgrade
+
+echo "--------- Installing GitLab dependencies ----------"
+fastInstall git
+
+echo "-------- Download Let's Encrypt ---------"
+sudo git clone https://github.com/letsencrypt/letsencrypt /opt/letsencrypt --depth=1
+
+echo "-------- Upgrade Let's Encrypt ---------"
+cd /opt/letsencrypt
+sudo git pull
+
+echo "-------- Launch Let's Encrypt ---------"
+sudo /opt/letsencrypt/letsencrypt-auto
+
+echo "- Install automatic Let's Encrypt renew (weekly) -"
+echo "sudo /opt/letsencrypt/certbot-auto renew --no-self-upgrade > /var/log/letsencrypt/letsencrypt-renew.log 2>&1" | sudo tee /etc/cron.weekly/letsencrypt-renew
+sudo chmod 755 /etc/cron.weekly/letsencrypt-renew
