ddd529ca0f2af12f238999674ff47f846cc26f59
Quentin Comte-Gaz <quentin@comte-gaz.com>
2017-10-01 20:38:46 +0200

    Linux: Update let's encrypt post install (new version)

--- Linux/post_install/last_install_lets_encrypt.sh
+++ Linux/post_install/last_install_lets_encrypt.sh
@@ -16,23 +16,34 @@ source ../utils/functions.sh
 echo "--------- Update and upgrade the system ----------"
 updateAndUpgrade
 
-echo "--------- Installing Let's Encrypt dependencies ----------"
-fastInstall git
+echo "--------- Installing Let's Encrypt ----------"
+fastInstall python-certbot-apache
 
-echo "-------- Download Let's Encrypt ---------"
-sudo git clone https://github.com/letsencrypt/letsencrypt /opt/letsencrypt --depth=1
+echo "-------- Create the certificates ---------"
+sudo certbot --apache
 
-echo "-------- Upgrade Let's Encrypt ---------"
-cd /opt/letsencrypt
-sudo git pull
-
-echo "-------- Launch Let's Encrypt ---------"
-sudo /opt/letsencrypt/letsencrypt-auto
-
-echo "- Install automatic Let's Encrypt renew (weekly) -"
+echo "- Install automatic Let's Encrypt renew (daily) -"
 echo "date +'----------------- Lets Encrypt cron began at %d-%m-%y at %H:%M:%S -----------------' >> /var/log/letsencrypt/letsencrypt-renew.log
-sudo /opt/letsencrypt/certbot-auto renew --no-self-upgrade >> /var/log/letsencrypt/letsencrypt-renew.log 2>&1
+sudo certbot renew >> /var/log/letsencrypt/letsencrypt-renew.log 2>&1
 sudo service mosquitto restart >> /var/log/letsencrypt/letsencrypt-renew.log 2>&1
 sudo service proftpd restart >> /var/log/letsencrypt/letsencrypt-renew.log 2>&1
 date +'----------------- Lets Encrypt cron done at %d-%m-%y at %H:%M:%S -----------------' >> /var/log/letsencrypt/letsencrypt-renew.log" | sudo tee /etc/cron.weekly/letsencrypt-renew
-sudo chmod 755 /etc/cron.weekly/letsencrypt-renew
+sudo chmod 755 /etc/cron.daily/letsencrypt-renew
+
+# Deprecated way to install Let's Encrypt certificates:
+#echo "--------- Installing Let's Encrypt dependencies ----------"
+#fastInstall git
+#echo "-------- Download Let's Encrypt ---------"
+#sudo git clone https://github.com/letsencrypt/letsencrypt /opt/letsencrypt --depth=1
+#echo "-------- Upgrade Let's Encrypt ---------"
+#cd /opt/letsencrypt
+#sudo git pull
+#echo "-------- Launch Let's Encrypt ---------"
+#sudo /opt/letsencrypt/letsencrypt-auto
+#echo "- Install automatic Let's Encrypt renew (weekly) -"
+#echo "date +'----------------- Lets Encrypt cron began at %d-%m-%y at %H:%M:%S -----------------' >> /var/log/letsencrypt/letsencrypt-renew.log
+#sudo /opt/letsencrypt/certbot-auto renew --no-self-upgrade >> /var/log/letsencrypt/letsencrypt-renew.log 2>&1
+#sudo service mosquitto restart >> /var/log/letsencrypt/letsencrypt-renew.log 2>&1
+#sudo service proftpd restart >> /var/log/letsencrypt/letsencrypt-renew.log 2>&1
+#date +'----------------- Lets Encrypt cron done at %d-%m-%y at %H:%M:%S -----------------' >> /var/log/letsencrypt/letsencrypt-renew.log" | sudo tee /etc/cron.weekly/letsencrypt-renew
+#sudo chmod 755 /etc/cron.weekly/letsencrypt-renew
